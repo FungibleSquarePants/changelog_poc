d6f02ff71e8724bde1f14dae8bd426f7189c61e2
Abhay Vaidya <avaidya@loyalty.com>
2018-06-26 16:53:28 -0400

    Fixed Jira data retrieval

--- script_file.js
+++ script_file.js
@@ -6,35 +6,32 @@
 //const WAFFLE_INFO = /\[connected to #\d+\]\s?/gi;
 const JIRA_STORY_NUMBER=/^FS-[0-9]+/
 
+function httpGet(theUrl)
+{
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.setRequestHeader("Authorization", "Basic ZnVuZ2libGU6ZnVuZ2libGUxMjM=");
+    xmlHttp.send();
+    return xmlHttp.responseText;
+}
+
 module.exports = function (data, callback) {
-  console.log(data);
-  const rewritten = data.commits.map((commit) => {
-    console.log("commit = ", commit)
+    const rewritten = data.commits.map((commit) => {
+    console.log("commit = ", commit);
     const story_number = commit.title.match(JIRA_STORY_NUMBER);
     console.log("story_number =", story_number);
-    //TODO get details of jira story that don't have a story number in the commit
+    commit.jira_summary = "DEFAULT";
     if (story_number) {
-      // extra metadata to remember the linked tasks
       commit.story_number = story_number[0];
       console.log(commit.story_number);
-      fetch("https://jira.loyalty.com/rest/api/2/issue/FS-600", {
-        method: 'GET',
-        headers: new Headers({
-          'Authorization': 'Basic yourkey_here',
-          'Content-Type': 'application/json'
-        })
-        /* mode: 'cors', // no-cors, cors, *same-origin
-          redirect: 'follow', // manual, *follow, error
-          referrer: 'no-referrer', // *client, no-referrer*/
-      }).then((resp) => {
-        console.log(resp);
-        commit.jira_summary = resp.json().fields.summary;
-        console.log(commit.jira_summary);
-      }).catch(err => {
-        console.log("hehrafadsf", err);
-      });
+      var response = JSON.parse(httpGet("https://jira.loyalty.com/rest/api/latest/issue/" + commit.story_number));
+      if (!response.errorMessages) {
+        if (response.fields.summary) {
+        commit.jira_summary = response.fields.summary;
+        }
+      }
     }
-
     return commit;
   });
 

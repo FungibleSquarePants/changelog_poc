12744232941e452dc279708bc242715a7c3b7ba9
Abhay Vaidya <avaidya@loyalty.com>
2018-06-27 10:44:32 -0400

    Modified post-processing script to also retrieve story descriptions and return unique stories

--- script_file.js
+++ script_file.js
@@ -1,34 +1,37 @@
-const JIRA_STORY_NUMBER=/^FS-[0-9]+/
+const JIRA_STORY_NUMBER = /^FS-[0-9]+/
 
 function httpGet(theUrl) {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
     xmlHttp.setRequestHeader("Authorization", "Basic ZnVuZ2libGU6ZnVuZ2libGUxMjM=");
     xmlHttp.send();
     return xmlHttp.responseText;
 }
 
 module.exports = function (data, callback) {
-    const rewritten = data.commits.map((commit) => {
-    const story_number = commit.title.match(JIRA_STORY_NUMBER);
-    commit.jira_summary = "DEFAULT";
-    if (story_number) {
-      commit.story_number = story_number[0];
-      var response = JSON.parse(httpGet("https://jira.loyalty.com/rest/api/latest/issue/" + commit.story_number));
-      if (!response.errorMessages) {
-        if (response.fields.summary) {
-          commit.jira_summary = response.fields.summary;
-        }
-      }
-    } else {
-      commit.story_number = "Story number does not exist in Jira.";
-    }
-    return commit;
-  });
+    var unique_stories = {};
 
-  callback({
-    commits: rewritten.filter(Boolean),
-    range: data.range.split('.')[0],
-  });
+    data.commits.forEach(function (commit) {
+        const story_number_matches = commit.title.match(JIRA_STORY_NUMBER);
+
+        // If the commit tile includes "FS-XXX"
+        if (story_number_matches) {
+            const story_number = story_number_matches[0];
+            const response = JSON.parse(httpGet("https://jira.loyalty.com/rest/api/latest/issue/" + story_number));
+
+            if (!response.errorMessages) {
+                unique_stories[story_number] = {
+                    summary: response.fields.summary,
+                    description: response.fields.description
+                };
+            }
+        }
+    });
+
+    callback({
+        commits: data.commits,
+        range: data.range,
+        stories: unique_stories
+    });
 }
